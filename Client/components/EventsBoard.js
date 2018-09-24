4b4253a7dbd5bb983dbf3227e57e19de98823630
Roy Koren <roykoren111@gmail.com>
2018-09-24 11:55:04 +0300

    Update EventsBoard.js

--- Client/components/EventsBoard.js
+++ Client/components/EventsBoard.js
@@ -1,81 +1,38 @@
 import React from 'react';
-import { View, ActivityIndicator, Button, ListView, FlatList, Text, Alert, StyleSheet, TouchableHighlight, InteractionManager, Platform } from 'react-native';
+import { View, FlatList, Button, Text, Platform, Alert } from 'react-native';
 import { MapView } from 'expo';
 
 import Event from './Event';
-import MapDisplay from './MapDisplay';
 import * as location_utils from '../utils/location';
 import * as utils from '../utils/utils';
 import { events as events_server_api } from '../utils/Server_api';
 
-import { StackNavigator, DrawerNavigator, createStackNavigator, createDrawerNavigator } from 'react-navigation';
-import { Ionicons, FontAwesome } from '@expo/vector-icons';
-import { nav } from './navigation_pages/Login';
-import styles from '../utils/styles';
-import * as storage_utils from '../utils/DataStorage';
-import { TextButton, RaisedTextButton } from 'react-native-material-buttons';
-
-var serverConnection;
-var ipAddress = 'http://vmedu158.mtacloud.co.il:8080/evee/webapi/events';
 
 export default class EventsBoard extends React.Component{
     constructor(props){
-        super(props);
-        const { navigation } = this.props;
-        const userID = navigation.getParam('id', '0');
-        const userName = navigation.getParam('username', '');
+        super(props)
         this.state = {
-            // user id + name
-            id: userID,
-            name: userName,
+            // user id
+            id: props.id,
+
             // public events
             events: [],
+
             // event kind is board/map according to user page. board = map, than kind='map'...
             event_kind: props.event_kind,
+
             // the event user chosed (relevant only for map view)
             chosed_event: {},
+
             // flag to indicate if should fetch events again from the server
             load_events: true,
+
             // filter options - the filters the user chose (could be another component, and sit in its state)
-            // if filter options will be a component, than this class is stateless
-            isLoading: true,
-			aroundMeButtonOpacity: 0.5,
-			boardButtonOpacity: 1,
-			disableEvents: false,
-			menuActive: false,
-			menuOpacity: 0.5
+            // if filter options will be a component, than this class is stateless //
         }
+
+
     }
-
-    boardButton() {
-		this.setState({aroundMeButtonOpacity: 0.5});
-		this.setState({boardButtonOpacity: 1});
-        this.setState({disableEvents: false});
-        this.setState({event_kind: 'board'});
-		this.setState({menuActive: false});
-		this.render();
-	}
-
-	aroundMeButton() {
-        console.log('in aroundMeButton');
-		this.setState({aroundMeButtonOpacity: 1});
-		this.setState({boardButtonOpacity: 0.5});
-        this.setState({disableEvents: true});
-        this.setState({event_kind: 'map'});
-		this.setState({menuActive: false});		
-		this.render();		
-	}
-	
-	filtersButton() {
-		Alert.alert("filters");
-	}
-
-	menuButton() {
-		this.setState({menuActive: !this.state.menuActive});
-		this.setState({menuOpacity: 1});
-		//this.props.navigation.openDrawer();
-		this.render();
-	}
 
     fetch_events = async () => {
         let tries = 0;
@@ -102,16 +59,16 @@ export default class EventsBoard extends React.Component{
                  })
             tries = tries + 1;
         }
-
-        console.log(events);
-        return events;
+        console.log(events)
+        return events
     }
 
     async componentDidMount(){
         if(!this.state.load_events){
-            return;
+            return
         }
-       events = await this.fetch_events();
+        
+       events = await this.fetch_events()
 
         // filter the events \\
         //============================= READ THIS BEFORE WORKING ON FILTER ==========================\\
@@ -128,179 +85,25 @@ export default class EventsBoard extends React.Component{
                 events: events,
                 event_kind: prev_state.event_kind,
                 chosed_event: null,
-                load_events: false
+                load_events: false,
             }
         })
+        
     }
 
     render(){
-        console.log(this.state);
-        var events = this._generate_events(this.state.events);
-
-        // why is undefined?????????
-        console.log('========================================='+this.state.event_kind);
-
-        if (this.state.menuActive){
-			return this.menuMode(events);
-		}
-		if (this.state.disableEvents){
-            // this.setState({event_kind: 'map'});            
-            // console.log(this.state.event_kind); 
-            console.log("mapMode");            
-            return this.aroundMeMode(events);
-		}
-		else {
-            // this.setState({event_kind: 'board'});            
-            // console.log(this.state.event_kind); 
-            console.log("boardMode");
-            return this.boardMode(events);
-		}
-    }
-
-    aroundMeMode(events){
-        return(
-            // <MapDisplay events={events}/>
-            <View style={styles.wholeApp}>
-				<View style={styles.topContent}>
-					<View style={{flex: 0.1, padding: 8}}>
-						<TouchableHighlight onPress={() => this.menuButton()} underlayColor={'transparent'}>
-							<Text style={{opacity: this.state.menuOpacity, margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
-							<FontAwesome name="user" size={24} />
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.4, padding: 8}}>
-						<TouchableHighlight onPress={() => this.boardButton()} underlayColor={'transparent'}
-						style={{opacity: this.state.boardButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
-							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
-							Board
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.4, padding: 8}}>
-						<TouchableHighlight onPress={() => this.aroundMeButton()} underlayColor={'transparent'}
-						style={{opacity: this.state.aroundMeButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
-							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
-							Around Me
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.1, padding: 8}}>
-						<TouchableHighlight onPress={() => this.filtersButton()} underlayColor={'transparent'}>
-							<Text style={{margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
-							<FontAwesome name="filter" size={24} />
-							</Text>
-						</TouchableHighlight>
-					</View>
-				</View>
-                <View style={styles.mainContent}>
-                    {events}
-                </View>
-			</View>
-        )
-    }
-
-    boardMode(events){
+        console.log(this.state)
+        var events = this._generate_events(this.state.events)
         return (
-            <View style={styles.wholeApp}>
-				<View style={styles.topContent}>
-					<View style={{flex: 0.1, padding: 8}}>
-						<TouchableHighlight onPress={() => this.menuButton()} underlayColor={'transparent'}>
-							<Text style={{opacity: this.state.menuOpacity, margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
-							<FontAwesome name="user" size={24} />
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.4, padding: 8}}>
-						<TouchableHighlight onPress={() => this.boardButton()} underlayColor={'transparent'}
-						style={{opacity: this.state.boardButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
-							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
-							Board
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.4, padding: 8}}>
-						<TouchableHighlight onPress={() => this.aroundMeButton()} underlayColor={'transparent'}
-						style={{opacity: this.state.aroundMeButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
-							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
-							Around Me
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.1, padding: 8}}>
-						<TouchableHighlight onPress={() => this.filtersButton()} underlayColor={'transparent'}>
-							<Text style={{margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
-							<FontAwesome name="filter" size={24} />
-							</Text>
-						</TouchableHighlight>
-					</View>
-				</View>
-                <View style={styles.mainContent}>
-                    {events}
-                </View>
-			</View>
+            <View>
+                {events}
+            </View>
         )
-    }
-
-    menuMode(events){
-		return(
-			<View style={styles.wholeApp}>
-				<View style={styles.topContent}>
-					<View style={{flex: 0.1, padding: 8}}>
-						<TouchableHighlight onPress={() => this.menuButton()} underlayColor={'transparent'}>
-							<Text style={{opacity: this.state.menuOpacity, margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
-							<FontAwesome name="user" size={24} />
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.4, padding: 8}}>
-						<TouchableHighlight onPress={() => this.boardButton()} underlayColor={'transparent'}
-						style={{opacity: this.state.boardButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
-							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
-							Board
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.4, padding: 8}}>
-						<TouchableHighlight onPress={() => this.aroundMeButton()} underlayColor={'transparent'}
-						style={{opacity: this.state.aroundMeButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
-							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
-							Around Me
-							</Text>
-						</TouchableHighlight>
-					</View>
-					<View style={{flex: 0.1, padding: 8}}>
-						<TouchableHighlight onPress={() => this.filtersButton()} underlayColor={'transparent'}>
-							<Text style={{margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
-							<FontAwesome name="filter" size={24} />
-							</Text>
-						</TouchableHighlight>
-					</View>
-				</View>
-                <Text style={{margin: 5, fontSize: 24, textAlign: 'center', color: '#77c8ce'}}>
-                    {this.state.name}
-                </Text>
-                <TextButton 
-                    title='Logout'
-                    onPress={this._delete_user}
-                    titleColor='white'
-                    color='green'
-                    style={{height:'10%'}}
-                />
-			</View>
-		);
-    }
-    
-    _delete_user = async () => {
-        await storage_utils.removeData('user_id');
-        await storage_utils.removeData('user_name');
-        await storage_utils.removeData('user_id');
-        console.log('deleted all user data');
-        nav.navigate('LoginScreen');
     }
 
     _generate_events = (events) => {
-        var chosed_event = this.state.chosed_event;
+        var chosed_event = this.state.chosed_event
+        
         if(this.state.event_kind == 'map'){
             var event_objects = events.map(event => {
                 var show_data = (chosed_event!=null && chosed_event.id == event.id)
@@ -311,7 +114,8 @@ export default class EventsBoard extends React.Component{
                                     event_kind={this.state.event_kind}
                                     chosed_event_cb={this.chosed_event_cb}
                                     register_to_event_cb={this.register_to_event} />
-            });
+            })
+            
             event_board = 
                 <View>
                     <MapView
@@ -319,14 +123,16 @@ export default class EventsBoard extends React.Component{
                     initialRegion={location_utils.get_current_location()}
                     showsUserLocation={true}
                     followUserLocation={true} >
+
                         {event_objects}
                     </MapView>
                     {Platform.OS == 'android' ? <Button title='register' onPress={this.register_to_event} disabled={this.state.chosed_event==null} /> : null}
                 </View>
         }
-        else {
+        else{
             if(this.state.events.length == 0) 
-                return null;
+                return null
+            
             event_board = 
                 <View style={{ flex: 1, alignItems: "center", justifyContent: "center", width: '150%' }}>
                     <FlatList   data={this.state.events}
@@ -351,7 +157,8 @@ export default class EventsBoard extends React.Component{
 
     chosed_event_cb = (event_name, event_id) => {
         current_chosed_event = this.state.chosed_event;
-        console.log(utils.string_format('user chosed event - {0}', event_name));
+        console.log(utils.string_format('user chosed event - {0}', event_name))
+
         var chosed_event = {};
         chosed_event['name'] = event_name;
         chosed_event['id'] = event_id
@@ -363,7 +170,7 @@ export default class EventsBoard extends React.Component{
                     events: prev_state.events,
                     event_kind: prev_state.event_kind,
                     chosed_event: chosed_event,
-                    load_events: prev_state.load_events
+                    load_events: prev_state.load_events,
                 }
             })
         }
