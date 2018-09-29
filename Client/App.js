59a1cd1a80cd91432181f9e3ff3a94d5a9791bd6
Roy Koren <roykoren111@gmail.com>
2018-09-29 12:29:40 +0300

    Navigation to main page after posting new event

--- Client/App.js
+++ Client/App.js
@@ -8,75 +8,20 @@ import FontAwesome, { Icons } from 'react-native-fontawesome';
 
 import Login from './components/navigation_pages/Login';
 import UserForm from './components/UserForm';
-import Hamburger1 from './hamburger1';
 import Board from './components/Board';
-
-//export { nav };
 
 // Global Variables
 var ipAddress = 'http://vmedu158.mtacloud.co.il:8080/evee/webapi/events/';
-var username = " ";
-var passwrd = " ";
-var eventDetails = " ";
-var nav; // local
 global.id = 0;
+global.username = "";
 
 const LoginScreen = ({navigation}) => (<Login navigation={navigation} />);
 
 const NewEventScreen = ({ navigation }) => (
 	<View style={{backgroundColor: '#E2FCFF', flex: 1}}>
-		<UserForm id={global.id} />
+		<UserForm id={global.id} username={global.username} />
 	</View>
 );
-
-const EventDetails = ({ nav }) => (
-	<View style={{backgroundColor: '#E2FCFF', flex: 1}}>
-		<Text>{global.id}</Text>
-	</View>
-);
-
-// class EventDetails extends Component {
-//   render() {
-//     /* 2. Get the param, provide a fallback value if not available */
-//     const  nav  = this.props;
-//     const id = nav.getParam('currentEventID', 'NO-ID');
-
-//     return (
-// 			<View style={{backgroundColor: '#E2FCFF', flex: 1}}>
-// 		<Text>id: {JSON.stringify(id)}</Text>
-// 	</View>
-// 		);
-// 	}
-//}
-
-function eventPost(navigation) {
-	if (eventDetails === " ") { // Event is empty - alert
-		Alert.alert(
-			'Empty event',
-			'Event must contain at least 1 character!',
-			[
-				{text: 'Try Again'},
-				{text: 'Back To Events Screen', onPress: () => nav.navigate('Board')},
-			],
-			{ cancelable: false }
-		)
-	}
-	else { // Event is NOT empty - post new event
-		fetch(ipAddress,
-		{
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				content: eventDetails,
-			}),
-		});
-		eventDetails = " ";
-		nav.navigate('Board'); // Back to main events screen
-	}
-}
 
 const RootNavigator = createStackNavigator({
   LoginScreen: {
@@ -97,12 +42,6 @@ const RootNavigator = createStackNavigator({
 		headerTitle: 'Board',
     },
 	},
-	EventDetails: {
-		screen: EventDetails,
-			navigationOptions: {
-			headerTitle: 'Events Details',
-			},
-		},
 });
 
 export default RootNavigator;
