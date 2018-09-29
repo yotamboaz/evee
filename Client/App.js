0e084e5d94a04f7ef07329e2ec12d78df734a4eb
Roy Koren <roykoren111@gmail.com>
2018-09-29 14:33:11 +0300

    Managed events update

--- Client/App.js
+++ Client/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { ActivityIndicator, ListView, Alert, Button, StyleSheet, TextInput, Text, View, ScrollView, Image } from 'react-native';
+import { ActivityIndicator, ListView, Alert, Button, StyleSheet, TextInput, Text, TextButton, View, ScrollView, Image } from 'react-native';
 
 // react-native navigation ref - https://reactnavigation.org/docs/en/getting-started.html
 import { StackNavigator, createStackNavigator } from 'react-navigation';
@@ -9,6 +9,7 @@ import FontAwesome, { Icons } from 'react-native-fontawesome';
 import Login from './components/navigation_pages/Login';
 import UserForm from './components/UserForm';
 import Board from './components/Board';
+import ManageEvents from './components/ManageEvents';
 
 // Global Variables
 var ipAddress = 'http://vmedu158.mtacloud.co.il:8080/evee/webapi/events/';
@@ -22,6 +23,27 @@ const NewEventScreen = ({ navigation }) => (
 		<UserForm id={global.id} username={global.username} />
 	</View>
 );
+
+export class ManageEventsScreen extends React.Component{
+	constructor(props){
+		super(props);
+		const { navigation } = this.props;
+		const userID = navigation.getParam('id', '0');
+		const _kind = navigation.getParam('kind', '');
+		this.state = {
+			id: userID,
+			kind: _kind
+		}
+	}
+
+	render(){
+		return(
+			<View>
+					<ManageEvents id={this.state.id} kind={this.state.kind}/>
+			</View>
+		);
+	}
+}
 
 const RootNavigator = createStackNavigator({
   LoginScreen: {
@@ -42,6 +64,12 @@ const RootNavigator = createStackNavigator({
 		headerTitle: 'Board',
     },
 	},
+	ManageEventsScreen: {
+		screen: ManageEventsScreen,
+			navigationOptions: {
+			headerTitle: 'Manage Events',
+			},
+		},
 });
 
 export default RootNavigator;
