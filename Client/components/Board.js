2eb355468c57f6e52d3827f9770268751641a2ea
Roy Koren <roykoren111@gmail.com>
2018-10-01 14:10:27 +0300

    filter by category + sub-category

--- Client/components/Board.js
+++ Client/components/Board.js
@@ -5,8 +5,10 @@ import { StackNavigator, DrawerNavigator, createStackNavigator, createDrawerNavi
 import styles from '../utils/styles';
 import EventsBoard from './EventsBoard';
 import ManageEvents from './ManageEvents';
+import Categories from './Categories';
 
 import { events as events_server_api } from '../utils/Server_api';
+import { formats as formats_api, events as events_api } from '../utils/Server_api';
 import * as utils from '../utils/utils';
 import * as storage_utils from '../utils/DataStorage';
 import { nav } from './navigation_pages/Login';
@@ -29,62 +31,105 @@ export default class Board extends React.Component{
 			aroundMeButtonOpacity: 0.5,
 			boardButtonOpacity: 1,
 			menuActive: false,
+			filterScreenActive: false,
             menuOpacity: 0.5,
 			event_kind: 'board',
 			// flag to indicate if should fetch events again from the server
 			load_events: true,
 			// the public events
-			events: []
+			events: [],
+			filteredEvents: [],
+			filterSettings: {active: false, category: 'Default', sub_category: 'Default'},
+			//////
+            form_objects: [],
+            categories: {},
+            selected_category: 'Default',
+            selected_sub_category: 'Default',
+            location: {},
+            current_form: {category: null, sub_category: null},
+            field_values: {},
+            invalid_fields: {},
+            registered_fields: ['Event Name'],
 		}
 		global.id = userID;
 		global.username = userName;
 	}
 
 	async componentDidUpdate(prev_props){
+
 		if(!this.state.load_events)
 			return;
 		
-			events = await this.fetch_events();
-			if(!events){
-				return;
-			}
-	 
-			 // filter the events \\
-			 //============================= READ THIS BEFORE WORKING ON FILTER ==========================\\
-			 // need to think if filttering process should be here,                                       \\
-			 // because user might change filter options, and refetching                                  \\
-			 // events each time user change filter is not scalable, and not officiant.                   \\
-			 // maybe add filtered_events to state, and present them if user chosed filter options.       \\
-			 //===========================================================================================\\
-			 //																							 \\
-			 //  filtered_events = this.filter_events(events)											 \\
-			 //===========================================================================================\\
+		events = await this.fetch_events();
+		filteredEvents = await this.filter_events(events);
+		// console.log('==== ==== update ==== ====');
+		// console.log(events);
+		// console.log('--------------------')
+		// console.log(filteredEvents); 
+		if(!events){
+			return;
+		}
+	
+			// filter the events \\
+			//============================= READ THIS BEFORE WORKING ON FILTER ==========================\\
+			// need to think if filttering process should be here,                                       \\
+			// because user might change filter options, and refetching                                  \\
+			// events each time user change filter is not scalable, and not officiant.                   \\
+			// maybe add filtered_events to state, and present them if user chosed filter options.       \\
+			//===========================================================================================\\
+			//																							 \\
+			//  filtered_events = this.filter_events(events)											 \\
+			//===========================================================================================\\
 
-			 this.setState(prev_state => {
-				return {
-					id: prev_state.id,
-					username: prev_state.username,
-					isLoading: false,
-					aroundMeButtonOpacity: prev_state.aroundMeButtonOpacity,
-					boardButtonOpacity: prev_state.boardButtonOpacity,
-					menuActive: prev_state.menuActive,
-					menuOpacity: prev_state.menuOpacity,
-					event_kind: prev_state.event_kind,
-					load_events: false,
-					events: events
-				}
-			})
+		this.setState(prev_state => {
+			return {
+				id: prev_state.id,
+				username: prev_state.username,
+				isLoading: false,
+				aroundMeButtonOpacity: prev_state.aroundMeButtonOpacity,
+				boardButtonOpacity: prev_state.boardButtonOpacity,
+				menuActive: prev_state.menuActive,
+				menuOpacity: prev_state.menuOpacity,
+				event_kind: prev_state.event_kind,
+				load_events: false,
+				events: events,
+				filteredEvents: filteredEvents
+			}
+		});
 	}
 	
 	async componentDidMount(){
-        if(!this.state.load_events){
-            return
+
+		if(!this.state.load_events){
+			return;
+		}
+		
+		events = await this.fetch_events();
+		filteredEvents = await this.filter_events(events);
+		// console.log('==== ==== mount ==== ====');
+		// console.log(events);
+		// console.log('--------------------')
+		// console.log(filteredEvents); 
+		if(!events){
+			return;
+		}
+
+		//?
+
+		var form_objects = null;
+		form_objects = await this._pull_forms();
+        if(!form_objects){
+            return;
         }
-        
-	   events = await this.fetch_events()
-	   if(!events){
-		   return;
-	   }
+        var categories = utils.collect_categories(form_objects);
+		console.log('--------------------- categories --------------------');
+        console.log(categories);
+
+        this.setState(prev_state => {
+            return {
+                categories: categories,
+            }
+        })
 
         // filter the events \\
         //============================= READ THIS BEFORE WORKING ON FILTER ==========================\\
@@ -108,42 +153,60 @@ export default class Board extends React.Component{
 				menuOpacity: prev_state.menuOpacity,
 				event_kind: prev_state.event_kind,
 				load_events: false,
-				events: events
+				events: events,
+				filteredEvents: filteredEvents
             }
-        })
-        
+        });
     }
 
     boardButton() {
 		this.setState({aroundMeButtonOpacity: 0.5});
 		this.setState({boardButtonOpacity: 1});
+		this.setState({menuOpacity: 0.5});
+		this.setState({filterOpacity: 0.5});				
         this.setState({event_kind: 'board'});
 		this.setState({menuActive: false});
+		this.setState({filterScreenActive: false});
 		this.render();
 	}
 
 	aroundMeButton() {
 		this.setState({aroundMeButtonOpacity: 1});
 		this.setState({boardButtonOpacity: 0.5});
+		this.setState({menuOpacity: 0.5});
+		this.setState({filterOpacity: 0.5});
         this.setState({event_kind: 'map'});
-		this.setState({menuActive: false});		
+		this.setState({menuActive: false});
+		this.setState({filterScreenActive: false});	
 		this.render();		
-	}
-	
-	filtersButton() {
-		Alert.alert("filters");
 	}
 
 	menuButton() {
-		this.setState({menuActive: !this.state.menuActive});
+		this.setState({aroundMeButtonOpacity: 0.5});
+		this.setState({boardButtonOpacity: 0.5});
 		this.setState({menuOpacity: 1});
-		//this.props.navigation.openDrawer();
+		this.setState({filterOpacity: 0.5});
+		this.setState({menuActive: true});
+		this.setState({filterScreenActive: false});
 		this.render();
     }
+	
+	filtersButton() {
+		this.setState({aroundMeButtonOpacity: 0.5});
+		this.setState({boardButtonOpacity: 0.5});
+		this.setState({menuOpacity: 0.5});
+		this.setState({filterOpacity: 1});
+		this.setState({menuActive: false});		
+		this.setState({filterScreenActive: true});		
+		this.render();
+	}
     
     render(){
         if (this.state.menuActive){
 			return this.menuMode();
+		}
+		else if (this.state.filterScreenActive){
+			return this.renderFilterOptions();
 		}
 		else {
             return this.renderEvents();
@@ -179,14 +242,14 @@ export default class Board extends React.Component{
 					</View>
 					<View style={{flex: 0.1, padding: 8}}>
 						<TouchableHighlight onPress={() => this.filtersButton()} underlayColor={'transparent'}>
-							<Text style={{margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
+							<Text style={{opacity: this.state.filterOpacity, margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
 							<FontAwesome name="filter" size={24} />
 							</Text>
 						</TouchableHighlight>
 					</View>
 				</View>
                 <View style={styles.mainContent}>
-                    <EventsBoard id={this.state.id} events={this.state.events} event_kind={this.state.event_kind}/>
+                    <EventsBoard id={this.state.id} events={this.state.filteredEvents} event_kind={this.state.event_kind}/>
                 </View>
 				<View style={styles.bottomContent}>
 					<TouchableHighlight onPress={() => {this.setState({load_events: true})}}>
@@ -234,7 +297,7 @@ export default class Board extends React.Component{
 					</View>
 					<View style={{flex: 0.1, padding: 8}}>
 						<TouchableHighlight onPress={() => this.filtersButton()} underlayColor={'transparent'}>
-							<Text style={{margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
+							<Text style={{opacity: this.state.filterOpacity, margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
 							<FontAwesome name="filter" size={24} />
 							</Text>
 						</TouchableHighlight>
@@ -267,6 +330,71 @@ export default class Board extends React.Component{
 			</View>
 		);
 	}
+
+	renderFilterOptions(){
+		return(
+			<View style={styles.wholeApp}>
+				<View style={styles.topContent}>
+					<View style={{flex: 0.1, padding: 8}}>
+						<TouchableHighlight onPress={() => this.menuButton()} underlayColor={'transparent'}>
+							<Text style={{opacity: this.state.menuOpacity, margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
+							<FontAwesome name="user" size={24} />
+							</Text>
+						</TouchableHighlight>
+					</View>
+					<View style={{flex: 0.4, padding: 8}}>
+						<TouchableHighlight onPress={() => this.boardButton()} underlayColor={'transparent'}
+						style={{opacity: this.state.boardButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
+							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
+							Board
+							</Text>
+						</TouchableHighlight>
+					</View>
+					<View style={{flex: 0.4, padding: 8}}>
+						<TouchableHighlight onPress={() => this.aroundMeButton()} underlayColor={'transparent'}
+						style={{opacity: this.state.aroundMeButtonOpacity, borderRadius:10, borderWidth: 1, borderColor: '#77c8ce'}}>
+							<Text style={{margin: 5, fontSize: 16, textAlign: 'center', color: '#77c8ce'}}>
+							Around Me
+							</Text>
+						</TouchableHighlight>
+					</View>
+					<View style={{flex: 0.1, padding: 8}}>
+						<TouchableHighlight onPress={() => this.filtersButton()} underlayColor={'transparent'}>
+							<Text style={{opacity: this.state.filterOpacity, margin: 5, fontSize: 24, textAlign: 'left', color: '#77c8ce'}}>
+							<FontAwesome name="filter" size={24} />
+							</Text>
+						</TouchableHighlight>
+					</View>
+				</View>
+				<Categories
+					selected_category={this.state.filterSettings.category}
+					selected_sub_category={this.state.filterSettings.sub_category}
+					categories={this.state.categories}
+					on_category_changed={this.on_category_changed}
+					on_sub_category_changed={this.on_sub_category_changed}
+				/> 
+				<TextButton 
+                    title='Activate Filters'
+                    onPress={() => this.setState({
+						filterSettings: {active: true, category: this.state.current_form.category, sub_category: this.state.current_form.sub_category}						
+					})}
+                    titleColor='white'
+                    color='blue'
+                    style={{height:'10%'}}
+                />
+				<TextButton 
+                    title='Display All Events'
+                    onPress={() => this.setState({
+						filteredEvents: this.state.events,
+						filterSettings: {active: false, category: 'Default', sub_category: 'Default'}
+					})}
+                    titleColor='white'
+                    color='red'
+                    style={{height:'10%'}}
+                />
+			</View>
+		);
+	}
 	
 	fetch_events = async () => {
         let tries = 0;
@@ -292,6 +420,101 @@ export default class Board extends React.Component{
             tries = tries + 1;
         }
         return events
+	}
+	
+	filter_events = async (events) => {
+		console.log('=========== filter_events ============');
+		var filteredEvents = [];
+		if (this.state.filterSettings.active){
+			var category = this.state.filterSettings.category;
+			var sub_category = this.state.filterSettings.sub_category;			
+			events.forEach(function(event){														
+				if (event.category == category && event.sub_category == sub_category){
+					filteredEvents.push(event);
+					console.log('event was added to filteredEvents');
+				}
+			})
+			console.log(filteredEvents);
+			return filteredEvents;
+		}
+		console.log(events);
+		return events;		
+	}
+
+	_pull_forms = async () => {
+        let tries = 0;
+        var forms = null;
+        while(!forms && tries < 5){
+            forms = await fetch(formats_api)
+                    .then(response => response.json())
+                    .then(server_response => {
+                        if(server_response.status == 'success'){
+                            return server_response.formats;
+                        }
+                        else{
+                            tries = 5;
+                            Alert.alert(server_Response.error);
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        if(tries >= 4){
+                            Alert.alert('could not fetch specific events forms from the server. please try again later...');
+                        }
+                    })
+            tries = tries + 1;
+        }
+        return forms;
+	}
+	
+	on_category_changed = (category) => {
+        this.setState(prev_state => {
+            return {
+				form_objects: prev_state.form_objects,
+				categories: prev_state.categories,
+				selected_category: category,
+				selected_sub_category: null,
+				location: prev_state.location,
+				current_form: {category: null, sub_category: null},
+				field_values: {},
+				invalid_fields: prev_state.invalid_fields,
+				registered_fields: prev_state.registered_fields,
+            }
+        })
+    }
+
+    on_sub_category_changed = (category, sub_category) => {
+        current_category = this.state.category;
+        current_sub_category = this.state.sub_category;
+        if(category==current_category && sub_category==current_sub_category)
+            // categories were not changed, thus no need to re-render
+            return
+
+        console.log(utils.string_format('changed categories to {0}\\{1}', category, sub_category))
+        let forms = this.state.form_objects;
+        var registered_fields = this.state.registered_fields.indexOf('Event Name') ? [] : ['Event Name']
+        forms.forEach(form_item => {
+            if(!(form_item.category == category && form_item.subCategory==sub_category))
+                return
+
+            form_item.fields.forEach(field => {
+                registered_fields.push(field.name)
+            })
+        })
+        this.setState(prev_state => {
+            return {
+					form_objects: prev_state.form_objects,
+					categories: prev_state.categories,
+					selected_category: category,
+					selected_sub_category: sub_category,
+					location: prev_state.location,
+					current_form: {category: category, sub_category: sub_category},
+					// Changed dynamic form, thus emptying the current one
+					field_values: {},
+					invalid_fields: prev_state.invalid_fields,
+					registered_fields: registered_fields
+                }
+		})
     }
 
     _delete_user = async () => {
